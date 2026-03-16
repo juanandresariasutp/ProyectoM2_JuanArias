10f06b3c0fb9c803817311d4e574860d25a833f7
JuanAndres2002 <juanandres.arias@utp.edu.co>
2026-03-16 14:09:25 -0500

    docs: agrega documentación OpenAPI y conecta Swagger UI

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from "express"
+import swaggerUi from "swagger-ui-express"
+import YAML from "yamljs"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 import authorsRoutes from "./routes/authors.routes.js"
 import postsRoutes from "./routes/posts.routes.js"
 import errorHandler from "./middleware/errorHandler.js"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const swaggerDocument = YAML.load(join(__dirname, "docs/openapi.yaml"))
+
 const app = express()
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use("/authors", authorsRoutes)
 app.use("/posts", postsRoutes)
